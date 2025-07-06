5fa4a7216b8a2e36ef5c649710587b70c804ec40
Khalil Hafz <khalilhafz@gmail.com>
2025-07-06 12:01:11 +0300

    initial setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,10 +1,13 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Header from "./api/components/Header";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body className="antialiased">
+      <Header />
+
         <Main />
         <NextScript />
       </body>
